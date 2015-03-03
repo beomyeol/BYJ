164dbb88296cd4853d039dd2e2ae98b21b6006c6
Beomyeol Jeon <junbr727@gmail.com>
2015-03-03 15:59:20 -0600

    Add thread

---
+++ include/byj/thread.h
@@ -0,0 +1,24 @@
+#ifndef __UTIL_THREAD__
+#define __UTIL_THREAD__
+
+#include <thread>
+
+namespace byj {
+    class thread {
+    public:
+        thread();
+        virtual ~thread();
+
+        virtual void run() = 0;
+
+        void start();
+        void join();
+        void detach();
+        bool joinable() const;
+
+    private:
+        std::thread thread_;
+    };
+}
+
+#endif
---
+++ src/thread.cpp
@@ -0,0 +1,23 @@
+#include "byj/thread.h"
+
+using namespace byj;
+
+thread::~thread() {
+    join();
+}
+
+void thread::start() {
+    thread_ = std::thread(&thread::run, this);
+}
+
+void thread::join() {
+    thread_.join();
+}
+
+void thread::detach() {
+    thread_.detach();
+}
+
+bool thread::joinable() const {
+    return thread_.joinable();
+}
